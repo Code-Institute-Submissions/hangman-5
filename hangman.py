5f08b0c64180c371ec5dd2c1e145b19826808376
Mat <mathew2801@gmail.com>
2021-08-03 13:33:24 +0000

    add a basic failed option to gameWon

--- hangman.py
+++ hangman.py
@@ -6,12 +6,14 @@ print(show)
 lives_remaining = 8
 gameWon = False
 
-while gameWon == False:
+while gameWon == False and lives_remaining > 0:
     guess = input('Type your guess using a letter or a word:')
     guess = guess.upper()
 
     if guess == word:
         gameWon = True
+    else:
+        lives_remaining -= 1
 
 if gameWon:
     print('Congratulations! You guessed the correct word. \n')
