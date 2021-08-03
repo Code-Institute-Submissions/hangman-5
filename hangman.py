113efaa1a6fd265bf456a15f2c61cfecd396de5a
Mat <mathew2801@gmail.com>
2021-08-03 13:45:41 +0000

    add show for each guess to show a correctly guessed letter

--- hangman.py
+++ hangman.py
@@ -2,16 +2,25 @@ import os, random, image, words
 
 word = 'BLANK'
 show = list(len(word)*'_')
-print(show)
 lives_remaining = 8
 gameWon = False
 
 while gameWon == False and lives_remaining > 0:
+    print(show)
     guess = input('Type your guess using a letter or a word:')
     guess = guess.upper()
 
     if guess == word:
         gameWon = True
+    if len(guess) == 1 and guess in word:
+        for letter in range(0,len(word)):
+            reveal = word[letter] 
+            if guess == reveal:
+                show[letter] = guess
+
+        if '_' not in show:
+            gameWon = True
+
     else:
         lives_remaining -= 1
 
