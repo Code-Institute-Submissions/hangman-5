94aee8c36ae0c3ca43b3f3b504073269f4b48632
Mat <mathew2801@gmail.com>
2021-08-03 13:29:46 +0000

    basic structure built for gameWon

--- hangman.py
+++ hangman.py
@@ -1,6 +1,20 @@
 import os, random, image, words
 
-word = 'blank'
+word = 'BLANK'
 show = list(len(word)*'_')
 print(show)
+lives_remaining = 8
+gameWon = False
+
+while gameWon == False:
+    guess = input('Type your guess using a letter or a word:')
+    guess = guess.upper()
+
+    if guess == word:
+        gameWon = True
+
+if gameWon:
+    print('Congratulations! You guessed the correct word. \n')
+else:
+    print(f'Unlucky you guessed incorrectly! The word was: {word}, try again')
 
