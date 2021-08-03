dbe0871ec6085e9d688c4eb2e79715fecad88cd5
Mat <mathew2801@gmail.com>
2021-08-03 13:49:40 +0000

    add commenting to hangman file

--- hangman.py
+++ hangman.py
@@ -1,15 +1,19 @@
+#Import files that will be used in my program
 import os, random, image, words
 
+#Variables used to crate the program
 word = 'BLANK'
 show = list(len(word)*'_')
 lives_remaining = 8
 gameWon = False
 
+#GameWon will become a function this does all the work checking if a letter or word we guessed is correct or incorrect. 
 while gameWon == False and lives_remaining > 0:
     print(show)
     guess = input('Type your guess using a letter or a word:')
     guess = guess.upper()
 
+# This section reveals a correctly guessed letter
     if guess == word:
         gameWon = True
     if len(guess) == 1 and guess in word:
@@ -20,10 +24,11 @@ while gameWon == False and lives_remaining > 0:
 
         if '_' not in show:
             gameWon = True
-
+# remove a life if a letter or word is guessed incorrectly.
     else:
         lives_remaining -= 1
 
+# win or lose statment
 if gameWon:
     print('Congratulations! You guessed the correct word. \n')
 else:
