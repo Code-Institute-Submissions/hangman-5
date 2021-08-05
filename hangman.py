63078edf0082da923cf845897415bc94ce40fa1a
Mat <mathew2801@gmail.com>
2021-08-05 11:17:32 +0000

    add commenting and fix errors

--- hangman.py
+++ hangman.py
@@ -1,10 +1,9 @@
 # Import files that will be used in my program
 import random
-from os import system, name
 from image import hanging_man
 from words import random_word
 
-#Variables used to crate the program
+# Variables used throught hangman.
 word = random.choice(random_word)
 word = word.upper()
 show = list(len(word)*'_')
@@ -21,43 +20,45 @@ def checking_guess(reveal, word):
     global show
 
     # This section reveals a correctly guessed letter
-    for letter in range(0,len(word)):
-        reveal = word[letter] 
+    for letter in range(0, len(word)):
+        reveal = word[letter]
         if guess == reveal:
             show[letter] = guess
-
+    # Show is the random word chosen from the words file
     if '_' not in show:
         return True
     else:
         return False
 
 
+# Clear the console to make things easier to read
 def clear_screen():
     """
-    This function clears the console each time a user makes a guess to keep the console 
-    clean.
+    This function clears the console each time a user makes a guess to keep
+    the console clean.
     """
     print('\033[H\033[J', end='')
 
 
+# Create the playing board this is waht users can see.
 def hangman_board():
+    """
+    Printing the users game board to the screen
+    this will include the hangman image, unrevealed word
+    and lives remaining.
+    """
     clear_screen()
     print(hanging_man[8-lives_remaining])
     print(" ".join([str(i) for i in show]))
     print(f'You have {lives_remaining} lives remaining!')
 
 
-
-#def gameWon():
-
-
-#def validation():
+# def validation():
 
 
-
-#GameWon will become a function this does all the work 
-#checking if a letter or word we guessed is correct or incorrect. 
-while gameWon == False and lives_remaining > 0:
+# GameWon will become a function this does all the work
+# checking if a letter or word we guessed is correct or incorrect.
+while gameWon is False and lives_remaining > 0:
     hangman_board()
     guess = input('Type your guess using a letter or a word:')
     guess = guess.upper()
@@ -66,12 +67,11 @@ while gameWon == False and lives_remaining > 0:
     if guess == word:
         gameWon = True
         show = word
-    if len(guess) == 1 and guess in word: 
+    if len(guess) == 1 and guess in word:
         gameWon = checking_guess(guess, word)
 # remove a life if a letter or word is guessed incorrectly.
     else:
         lives_remaining -= 1
-    
     hangman_board()
 
 # win or lose statment
