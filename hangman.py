98a1997c86e24b5ed66086626ecf7f41e2371431
Mat <mathew2801@gmail.com>
2021-08-05 09:18:35 +0000

    add function for checking a guess and showing a guessed letter

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-#Import files that will be used in my program
+# Import files that will be used in my program
 import os, random, image, words
 
 #Variables used to crate the program
@@ -7,7 +7,22 @@ show = list(len(word)*'_')
 lives_remaining = 8
 gameWon = False
 
-#GameWon will become a function this does all the work checking if a letter or word we guessed is correct or incorrect. 
+
+def checking_guess(reveal, word):
+    global show
+    # This section reveals a correctly guessed letter
+    for letter in range(0,len(word)):
+        reveal = word[letter] 
+        if guess == reveal:
+            show[letter] = guess
+
+    if '_' not in show:
+        return True
+    else:
+        return False
+
+#GameWon will become a function this does all the work 
+#checking if a letter or word we guessed is correct or incorrect. 
 while gameWon == False and lives_remaining > 0:
     print(show)
     guess = input('Type your guess using a letter or a word:')
@@ -16,14 +31,8 @@ while gameWon == False and lives_remaining > 0:
 # This section reveals a correctly guessed letter
     if guess == word:
         gameWon = True
-    if len(guess) == 1 and guess in word:
-        for letter in range(0,len(word)):
-            reveal = word[letter] 
-            if guess == reveal:
-                show[letter] = guess
-
-        if '_' not in show:
-            gameWon = True
+    if len(guess) == 1 and guess in word: 
+        gameWon = checking_guess(guess, word)
 # remove a life if a letter or word is guessed incorrectly.
     else:
         lives_remaining -= 1
@@ -33,4 +42,3 @@ if gameWon:
     print('Congratulations! You guessed the correct word. \n')
 else:
     print(f'Unlucky you guessed incorrectly! The word was: {word}, try again')
-
